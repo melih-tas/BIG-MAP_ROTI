d555ef67782de6c9c0e806396e889a07f3c01530
Melih Tas <101114939+melih-tas@users.noreply.github.com>
2022-04-27 11:29:59 +0200

    Add files via upload

---
+++ python_scripts/heating.py
@@ -0,0 +1,14 @@
+import requests
+import os.path
+from os import path
+
+session = requests.Session()
+session.auth = ('rw', 'jQ4AmRNz')
+
+session.verify = False
+
+rotavapor_ip = '169.254.149.76'
+base_url = f"https://{rotavapor_ip}/api/v1"
+process_endpoint = base_url + "/process"
+set_heating_msg = { 'heating' : { 'set' : int(input()) }}
+set_heating_resp = session.put(process_endpoint, json=set_heating_msg)
